05d85dd86ebb84301ad1f8471777c96bb156d9d2
applicant <applicant@Lenovo-Z70-80>
2017-03-24 17:07:32 +0100

    Team services for CRUD operations added

--- be_applicant/src/main/java/de/etherapists/beapplicant/restService/AppController.java
+++ be_applicant/src/main/java/de/etherapists/beapplicant/restService/AppController.java
@@ -7,16 +7,16 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
 
 import util.BasicController;
 
-@RestController
-@RequestMapping(value = "/test")
-public class TestController extends BasicController {
+@Controller
+@RequestMapping(value = "/app")
+public class AppController extends BasicController {
 
     @Autowired
     private HttpServletRequest request;
@@ -26,7 +26,7 @@ public class TestController extends BasicController {
 
     @ResponseBody
     @RequestMapping(value = "/test", method = RequestMethod.GET)
-    public void exerciseDone() throws IOException {
+    public void test() throws IOException {
 
         final Team team = new Team();
 
