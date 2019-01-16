62a10df408ac77368a597abfda946f993a8a8fa4
emad-gamal <emad.gamal33@gmail.com>
2019-01-16 14:14:46 +0100

    Stable V.1.0

--- js/cat-api-app/src/app/app.component.ts
+++ js/cat-api-app/src/app/app.component.ts
@@ -7,13 +7,25 @@ import { Component, OnInit  } from '@angular/core';
 })
 export class AppComponent implements OnInit{
 
-  cats: Cat =[];
+  cats;
   cat;
   title;
 
   ngOnInit() {
 
-    this.cats = [{
+    this.cats=[{
+        "breeds": [],
+        "categories": [
+          {
+          "id": 2,
+          "name": "space"
+          }
+        ],
+        "id": "4v",
+        "url": "https://cdn2.thecatapi.com/images/4v.jpg"
+      }];
+
+    this.cats.push({
     "breeds": [],
     "categories": [
     {
@@ -23,17 +35,18 @@ export class AppComponent implements OnInit{
     ],
     "id": "4v",
     "url": "https://cdn2.thecatapi.com/images/4v.jpg"
-    },
+    }
+    ,
     {
     "breeds": [],
     "id": "20f",
     "url": "https://cdn2.thecatapi.com/images/20f.png"
     },
     {
-    "breeds": [],
     "id": "4bv",
     "url": "https://cdn2.thecatapi.com/images/4bv.gif"
-    }]
+  }
+)
 
     this.cat = {
       "breeds": [],
--- js/cat-api-app/src/app/cat.ts
+++ js/cat-api-app/src/app/cat.ts
@@ -1,7 +1,10 @@
+import {Category} from './category'
+
 export class Cat{
 
   id: string;
   url: string;
   categories:Category[];
+  breeds: string[];
 
 }
